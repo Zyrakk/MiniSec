2b5a2941302bef5dc888edb53e2e67df506d4400
Zyrakk <150195749+Zyrakk@users.noreply.github.com>
2025-01-29 23:02:36 +0100

    Keylogger V1.2

--- keylogger/input_rec.py
+++ keylogger/input_rec.py
@@ -18,6 +18,6 @@ def on_release(key):
     if key == keyboard.Key.esc:
         return False
 
-# Listener
+# Listener de las teclas
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
